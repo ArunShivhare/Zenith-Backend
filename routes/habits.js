676e9468eb8dcfeed3088c078c81e55f09575b9e
Arun Shivhare <arunshivhare925@gmail.com>
2026-04-23 22:03:21 +0530

    fix habbit

--- routes/habits.js
+++ routes/habits.js
@@ -22,16 +22,22 @@ router.post("/", verifyToken, async (req, res) => {
 
 // DELETE habit
 router.delete("/:id", verifyToken, async (req, res) => {
-  await Habit.findByIdAndDelete(req.params.id);
+  await Habit.findOneAndDelete({
+    _id: req.params.id,
+    userId: req.user.uid,
+  });
   res.json({ msg: "Habit deleted" });
 });
 
 // UPDATE habit title
 router.put("/:id", verifyToken, async (req, res) => {
-  const habit = await Habit.findByIdAndUpdate(
-    req.params.id,
+  const habit = await Habit.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      userId: req.user.uid,
+    },
     { title: req.body.title },
-    { new: true }
+    { new: true },
   );
 
   res.json(habit);
@@ -41,14 +47,15 @@ router.put("/:id", verifyToken, async (req, res) => {
 router.put("/:id/toggle", verifyToken, async (req, res) => {
   const today = new Date().toISOString().split("T")[0];
 
-  const habit = await Habit.findById(req.params.id);
+  const habit = await Habit.findOne({
+    _id: req.params.id,
+    userId: req.user.uid,
+  });
 
   if (!habit) return res.status(404).json({ msg: "Not found" });
 
   if (habit.completedDates.includes(today)) {
-    habit.completedDates = habit.completedDates.filter(
-      (d) => d !== today
-    );
+    habit.completedDates = habit.completedDates.filter((d) => d !== today);
   } else {
     habit.completedDates.push(today);
   }
